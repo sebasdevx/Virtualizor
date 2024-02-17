72b2c716bb6f0c578aa1f665b426445578729304
X Sebas.dev X <137558871+sebasdevx@users.noreply.github.com>
2024-02-17 15:19:45 -0400

    Add files via upload

---
+++ virtualizor.sh
@@ -0,0 +1,31 @@
+if (( $EUID != 0 )); then
+    echo ""
+    echo "Please run as root"
+    echo ""
+    exit
+fi
+
+clear
+
+GitHub_Account="https://raw.githubusercontent.com/sebasdevx/Pterodactyl/main
+
+installVirtualizor() {
+    wget -N http://files.virtualizor.com/install.sh
+    chmod 0755 install.sh
+    ./install.sh email=your@email.com kernel=kvm
+}
+
+echo ""
+echo "[0] Exit"
+echo "[1] Install Virtualizor"
+echo ""
+read -p "Please enter a number: " choice
+echo ""
+
+installVirtualizor
+    clear
+    echo ""
+    echo -e "\033[0;92mphpMyAdmin installed successfully\033[0m"
+    echo ""
+    exit
+fi
